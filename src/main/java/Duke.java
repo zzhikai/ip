ac60ae8cc055936313c220c7c80aba170a28183c
Zhang Zhikai <zhangzhikai71@gmail.com>
2022-01-20 04:05:50 +0800

    Messy code implementation of level 2 Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,31 @@
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-
+        String[] inputStore = new String[100];
         String input;
+        int index = 1;
         // System.in.read() read one byte in the input stream
         // System.in() returns an input stream
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         Scanner chatInput = new Scanner( System.in );
+
         input = chatInput.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 1; i < index; i++ ) {
+                    System.out.println(i + "." + " " + inputStore[i]);
+                }
+                input = chatInput.nextLine();
+            }
+            if (input.equals("bye")) {
+                break;
+            }
+            inputStore[index++] = input;
+            System.out.println("added: " + input);
             input = chatInput.nextLine();
         }
+
         System.out.print("Bye. Hope to see you again soon!");
         chatInput.close();
     }
