521cb3ec0eb9864db25af9adddb509aedcb804a2
Zhang Zhikai <zhangzhikai71@gmail.com>
2022-01-20 05:55:54 +0800

    Level 4. ToDos, Events, Deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,14 +10,44 @@ public class Duke {
 
         Scanner chatInput = new Scanner( System.in );
 
+        // commands: todo, deadline, event
+
         input = chatInput.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 for (int i = 0; i < index; i++ ) {
-                    System.out.println((i + 1) + ". "+ "[" + inputStore[i].getStatusIcon() + "] "  + inputStore[i].description);
+                    // System.out.println((i + 1) + ". "+ "[" + inputStore[i].getStatusIcon() + "] "  + inputStore[i].description);
+                    System.out.println((i + 1) + "."+ inputStore[i].toString());
                 }
                 input = chatInput.nextLine();
                 // move to next iteration
+                continue;
+            }
+            if (input.contains("deadline")) {
+                Deadline task = new Deadline(input.substring(indexOfTask("deadline"), endIndexOfTask(input)), input.substring(endIndexOfTask(input) + 4));
+                inputStore[index++] = task;
+                addTaskMessage();
+                System.out.println("  " + task.toString());
+                printListLengthMessage(index);
+                input = chatInput.nextLine();
+                continue;
+            }
+            if (input.contains("todo")) {
+                Todo task = new Todo(input.substring(indexOfTask("todo")));
+                inputStore[index++] = task;
+                addTaskMessage();
+                System.out.println("  " + task.toString());
+                printListLengthMessage(index);
+                input = chatInput.nextLine();
+                continue;
+            }
+            if (input.contains("event")) {
+                Event task = new Event(input.substring(indexOfTask("event"), endIndexOfTask(input)), input.substring(endIndexOfTask(input) + 4));
+                inputStore[index++] = task;
+                addTaskMessage();
+                System.out.println("  " + task.toString());
+                printListLengthMessage(index);
+                input = chatInput.nextLine();
                 continue;
             }
             if (input.contains("unmark")) {
@@ -26,7 +56,7 @@ public class Duke {
                 int taskIndex = taskNumber - 1;
                 inputStore[taskIndex].unMark();
                 unmarkMessage();
-                System.out.println("[" + inputStore[taskIndex].getStatusIcon() + "] "  + inputStore[taskIndex].description);
+                System.out.println(inputStore[taskIndex].toString());
                 input = chatInput.nextLine();
                 continue;
             }
@@ -36,12 +66,12 @@ public class Duke {
                 int taskIndex = taskNumber - 1;
                 inputStore[taskIndex].markAsDone();
                 markMessage();
-                System.out.println("[" + inputStore[taskIndex].getStatusIcon() + "] "  + inputStore[taskIndex].description);
+                System.out.println(inputStore[taskIndex].toString());
                 input = chatInput.nextLine();
                 continue;
             }
-            inputStore[index++] = new Task(input);
-            System.out.println("added: " + input);
+            // inputStore[index++] = new Task(input);
+            // System.out.println("added: " + input);
             input = chatInput.nextLine();
         }
 
@@ -60,5 +90,23 @@ public class Duke {
 
     public static void unmarkMessage() {
         System.out.println("OK, I've marked this task as not done yet:");
+    }
+
+    public static int indexOfTask(String command) {
+        int index = command.length() + 1;
+        return index;
+    }
+
+    public static int endIndexOfTask(String input) {
+        int index = input.indexOf("/");
+        return index;
+    }
+
+    public static void addTaskMessage() {
+        System.out.println("Got it. I've added this task:");
+    }
+
+    public static void printListLengthMessage(int runningIndex) {
+        System.out.println(String.format("Now you have %d tasks in the list", runningIndex));
     }
 }
