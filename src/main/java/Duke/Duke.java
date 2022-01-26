221cf5f6425e1d41febf3382eda578728877f7ce
Zhang Zhikai <zhangzhikai71@gmail.com>
2022-01-27 01:16:08 +0800

    Clean up code

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -13,12 +13,10 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.time.DateTimeException;
 
 
 public class Duke {
 
-    // static String path = "src/main/java/duke.txt";
     private static Ui ui;
     private Ui uiDuke;
     private TaskList taskDataList;
@@ -26,7 +24,6 @@ public class Duke {
         ArrayList<Task> inputDatabase = (ArrayList<Task>) Storage.readFile();
         taskDataList = new TaskList(inputDatabase);
     }
-
 
     private static void printFileContents(String filePath) throws FileNotFoundException {
         File f = new File(filePath);
@@ -55,65 +52,11 @@ public class Duke {
         Storage.saveToFile(this.taskDataList.getTaskStore());
         ui.bye();
         chatInput.close();
-
-        // write into file
-        /*try {
-            fileStore.saveToFile(this.taskDataList.getTaskStore());
-            ui.bye();
-            chatInput.close();
-        } catch (Exception e) {
-            ui.bye();
-            chatInput.close();
-        }*/
-        /* try {
-            ArrayList<Task> storeList = this.taskDataList.getTaskStore();
-            System.out.println(storeList.get(0).toString());
-            fileStore.saveToFile(storeList);
-        } catch (NullPointerException d) {
-            ui.bye();
-            chatInput.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e);
-        }
-        ui.bye();
-        chatInput.close();*/
-
     }
 
     public static void main(String[] args) throws DukeException {
         Duke chatBot = new Duke("src/main/java/TaskDatabase.ser");
         chatBot.run();
-        // Array of Duke.Task.Duke.Task instead, each task has its state and behaviour
-        /*ArrayList<Task> inputStore = new ArrayList<Task>();
-        TaskList taskDataList;
-        String input;
-        // ui = new Ui();
-        int index = 0;
-
-        ui.hello();
-        Storage fileStore = new Storage(path);
-        ArrayList<Task> inputDatabase = (ArrayList<Task>) fileStore.readFile();
-        taskDataList = new TaskList(inputDatabase);
-        Scanner chatInput = new Scanner( System.in );
-
-
-        input = chatInput.nextLine();
-
-        while (!input.equals("bye")) {
-            try {
-                Parser inputParser = new Parser(input);
-                Command currCommand = inputParser.parse();
-                currCommand.callCommand(taskDataList);
-            } catch (DukeException e) {
-                ui.showError(e);
-            }
-            input = chatInput.nextLine();
-        }
-        // write into file
-        fileStore.saveToFile(taskDataList.getTaskStore());
-        ui.bye();
-        chatInput.close();*/
     }
 
     public static void writeDataInputToDisk(ArrayList<Task> inputTaskList) {
