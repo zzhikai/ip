e4e1e847af1c01ccfac2bd004252a6b5e180d5b5
Zhang Zhikai <zhangzhikai71@gmail.com>
2022-01-28 00:39:51 +0800

    Add JavaDocs for Parser class

--- src/main/java/Duke/Parser/Parser.java
+++ src/main/java/Duke/Parser/Parser.java
@@ -1,27 +1,39 @@
 package Duke.Parser;
 
-import Duke.Exception.*;
-import Duke.Task.*;
 import Duke.Command.*;
-import Duke.TaskList.TaskList;
+import Duke.Exception.DukeException;
+import Duke.Exception.InvalidCommandException;
 
+/**
+ * Represents the parser for program to parse input.
+ */
 public class Parser {
     // Parser to return the action to do maybe the command to do
     // input : String
     // from that input we will split the words into command / description / datetime
     private String userInputLine;
+
+    /**
+     * Creates new Parser.
+     *
+     * @param userInputLine UserInputLine given for parser to parse.
+     */
     public Parser(String userInputLine) {
         this.userInputLine = userInputLine;
-        // this.taskList = taskList;
     }
-    // parse should throw exception or command should throw exception, each command has its condition
+
+    /**
+     * Returns Command that user wants to execute.
+     *
+     * @return Command to be executed on a TaskList.
+     * @throws DukeException If userInputLine invalid for the command.
+     */
     public Command parse() throws DukeException {
         // split command away
         String[] inputStrings = this.userInputLine.trim().split(" ",2);
         String inputCommand = inputStrings[0];
         // has command and body
         String inputBody = inputStrings.length == 2 ? inputStrings[1] : "";
-        Task task;
         switch (inputCommand) {
             case "list":
                 return new ListCommand("");
