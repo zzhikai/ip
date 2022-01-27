7c1e250aca6e5d1395ea8c7280fa0314156a1ae6
Zhang Zhikai <zhangzhikai71@gmail.com>
2022-01-27 23:45:51 +0800

    Add JavaDocs for classes in Exception package

--- src/main/java/Duke/Exception/EmptyEventAtException.java
+++ src/main/java/Duke/Exception/EmptyEventAtException.java
@@ -1,5 +1,8 @@
 package Duke.Exception;
 
+/**
+ * Represents an exception thrown when Event task does not have date.
+ */
 public class EmptyEventAtException extends DukeException{
     public EmptyEventAtException(String errorMessage) {
         super(errorMessage);
